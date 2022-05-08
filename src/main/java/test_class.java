efc3d1d4d0b5037191a247866ebd0f80a6e53f8a
Micaela Firpo <mdatofirpo@gmail.com>
2022-05-08 16:19:38 -0300

    Test cambio de user Git

--- src/main/java/test_class.java
+++ src/main/java/test_class.java
@@ -4,5 +4,6 @@ public class test_class {
 	public static void main(String[] args) {
 		System.out.print("Hola");
 		System.out.println("que onda wey");
+		System.out.println("Probando cambio de user Git");
 	}
 }
